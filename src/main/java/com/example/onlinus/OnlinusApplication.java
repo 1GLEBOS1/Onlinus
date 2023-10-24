07c7e70c73855cbc77b08cd721240a51dab3e02f
Gleb Polyakov <76563597+1GLEBOS1@users.noreply.github.com>
2023-10-25 00:12:06 +0300

    Initial commit

---
+++ src/main/java/com/example/onlinus/OnlinusApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinus/OnlinusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
