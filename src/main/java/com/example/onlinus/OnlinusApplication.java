cc63fcae5426eb4418b04694157c9b3e15312b7a
Gleb Polyakov <76563597+1GLEBOS1@users.noreply.github.com>
2023-10-26 17:56:17 +0300

    Add dependencies

--- src/main/java/com/example/onlinus/OnlinusApplication.java
+++ src/main/java/com/example/onlinus/OnlinusApplication.java
@@ -1,13 +1,25 @@
 package com.example.onlinus;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlinusApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OnlinusApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+
+		};
 	}
 
 }
